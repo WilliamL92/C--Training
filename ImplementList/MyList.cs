ef585dfec21bee2da7fe7c7a2beee49bdf7147fb
William <lavit.william@gmail.com>
2025-03-03 18:47:39 +0100

    feat(ImplementList): setup project and a moking List system in .NET

---
+++ ImplementList/MyList.cs
@@ -0,0 +1,82 @@
+﻿namespace ImplementList
+{
+    public class MyList<T>
+    {
+        private T[] _list = [];
+        private int _defaultCapacity = 4;
+        private int _size = 0;
+        public MyList()
+        {
+
+        }
+
+        public int Capacity {  
+            get { return _size; }
+            set
+            {
+                if (value > _list.Length)
+                {
+                    T[] tempArray = new T[value];
+                    Array.Copy(_list, tempArray, value);
+                    _list = tempArray;
+                }
+                _size = value;
+            }
+        }
+
+        public void Add(T elem)
+        {
+            if(_size == 0)
+            {
+                Capacity = _defaultCapacity;
+            }
+            else
+            {
+                _size++;
+                if(_size > Capacity)
+                {
+                    Capacity *= 2;
+                }
+            }
+            _list[_size] = elem;
+            _size++;
+        }
+
+        public void Remove(T elem)
+        {
+            int cursor = -1;
+            bool elemFind = false;
+            for (int i = 0; i < _list.Length; i++)
+            {
+                if (_list[i].Equals(elem) && !elemFind)
+                {
+                    cursor = i;
+                    elemFind = true;
+                }
+                else if (cursor != -1)
+                {
+                    _list[i - 1] = _list[i];
+                }
+            }
+            if (elemFind)
+            {
+                Capacity--;
+                _size--;
+            }
+        }
+
+        public int Count
+        {
+            get { return _list.Length; }
+
+        }
+        public T GetItems(int index)
+        {
+            if (index >= 0)
+            {
+                return _list[index];
+            }
+            throw new ArgumentException($"Index should be >= 0, received: {index}");
+        }
+    }
+}
---
+++ ImplementList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MyList<T>
+{
+    private T[] _list = [];
+    private int _index = 0;
+    public MyList()
+    {
+        
+    }
+
+    public void Add(T elem)
+    {
+        Array.Resize(ref _list, _list.Length + 1);
+        _list[_index] = elem;
+        _index++;
+    }
+
+    public void Remove(T elem) 
+    {
+        T[] tempArr = new T[_list.Length];
+        Array.Copy(_list, tempArr, _list.Length);
+        int cursor = -1;
+        bool elemFind = false;
+        for (int i = 0; i < tempArr.Length; i++) 
+        {
+            if (tempArr[i].Equals(elem) && !elemFind)
+            {
+                cursor = i;
+                elemFind = true;
+            }
+            else if (cursor != -1) {
+                _list[i - 1] = tempArr[i];
+            }
+        }
+        if (elemFind)
+        {
+            Array.Resize(ref _list, _list.Length - 1);
+            _index--;
+        }
+    }
+
+    public int Count
+    {
+        get { return _list.Length; }
+       
+    }
+    public T GetItems(int index)
+    {
+        return _list[index];
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    { 
+    }
+}
